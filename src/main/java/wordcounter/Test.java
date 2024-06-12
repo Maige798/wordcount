4c395c6244f5e921681d3fe7e58d194b2453c36b
Maige798 <3317991292@qq.com>
2024-06-12 14:57:05 +0800

    test

---
+++ src/main/java/wordcounter/Test.java
@@ -0,0 +1,9 @@
+package wordcounter;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("this is a test method/n" + Arrays.toString(args));
+    }
+}
---
+++ src/main/java/wordcounter/WordCountDriver.java
@@ -0,0 +1,34 @@
+package wordcounter;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCountDriver {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: WordCountDriver <input path> <output path>");
+            System.exit(-1);
+        }
+
+        Configuration conf = new Configuration();
+        conf.set("mapreduce.framework.name", "local");
+        Job job = Job.getInstance(conf);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setJarByClass(WordCountDriver.class);
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
